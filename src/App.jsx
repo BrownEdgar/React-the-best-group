b08bd501aba6e4beae96bd33c0aee20ee69ed6b1
BrownEdgar <nikoghosjanedgar@gmail.com>
2023-12-16 21:55:53 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Users from './components/Users/Users'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Lorem ipsum dolor sit amet.</h1>
+      <Users title="user post N1" />
+    </div>
+  )
+}
+
+export default App
