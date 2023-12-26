5485acff86d2f27921db31c84f56c16dabac014e
ArmanStep00 <armanstepanyan85@gmail.com>
2023-12-26 18:13:51 +0400

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,21 @@ import Users from './components/Users/Users'
 import { useState } from 'react'
 
 function App() {
-  const components = [
-    < Posts />, <Todos />, <Comments />, <Users />
-  ];
-  const componentTitles = ['Our Posts', 'To Do List', 'Our Comments', 'Our Users'];
-  const [page, setPage] = useState(0);
-  const nextPage = () => {
-    if (page + 1 === components.length) { setPage(0); }
-    else { setPage(page + 1); }
-  }
-  const prevPage = () => {
-    if (page === 0) { setPage(components.length - 1); }
-    else { setPage(page - 1); }
-  }
-  return (<div className='App'>
-    <h2 className='App__title'>{componentTitles[page]}</h2>
-    <div className='App__content'>
-      <i className="bi bi-arrow-left-circle-fill App__btn App__btn-prev" onClick={prevPage}></i>
-      {components[page]}
-      <i className="App__btn App__btn-prev bi bi-arrow-right-circle-fill " onClick={nextPage}></i>
+  
+  return (
+    <div className='App'>
+      <h2>Posts</h2>
+      <Posts />
+      <h2>Todos</h2>
+      <Todos />
+      <h2>Comments</h2>
+      <Comments />
+      <h2>Users</h2>
+      <Users />
+  
     </div>
-  </div>
-
-  )
+  
+    )
 }
 
 export default App
