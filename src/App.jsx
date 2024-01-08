474e1c31210b898084b8588b33a96a208f95af7d
ArmenParavyan <armenparavyan@gmail.com>
2024-01-08 20:34:26 +0400

    films

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Posts from './components/Posts/Posts.jsx';
 import Form from './components/Form/Form.jsx';
+import SelectFilm from './components/SelectFilm/SelectFilm.jsx';
 
 function App() {
   
   return (
     <div className='App'>
+      <SelectFilm />
       <Form />
       <Posts />
     </div>
