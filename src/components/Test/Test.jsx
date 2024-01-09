c55a67d02c8244266cc1728ec8406c7b4a7e46ac
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-01-09 22:22:14 +0400

    react repetition 2024

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import { useState } from 'react'
+import Title from '../Title/Title'
 
 export default function Test() {
+
   return (
     <div>
+      <Title title="repetition" />
 
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/Forms/example2/App'
+import App from './components/PropTypes/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
