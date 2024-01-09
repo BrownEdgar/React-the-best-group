8ebe6e56bb0fac492e8b13e1d8ed814193050b85
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-01-09 20:37:25 +0400

    added forms example

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/Forms/App'
+import App from './components/Forms/example2/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
