b3c8413e7c7223886d901a6b117aff89fd277230
MLEVARM <levon.marutyan1998@gmail.com>
2024-01-13 19:31:09 +0400

    ARNO'S HOMEWORK

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './components/Formik/App'
+import App from './components/Users/Users'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
