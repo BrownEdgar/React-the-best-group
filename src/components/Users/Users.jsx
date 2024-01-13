bbc80ff8799b167ff6e7aaf2b06daebce15a6914
ArnoSahakyan <sahakyan.arno@gmail.com>
2024-01-13 17:56:30 +0400

    Formik homework

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,60 +1,68 @@
 import './Users.scss'
-import instance from '../axios/axios'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { object, string } from 'yup'
+import { nanoid } from 'nanoid'
+
+
+const initialValues = {
+  username: '',
+  password: '',
+}
+
+const validationSchema = object({
+  username: string().min(3).max(15).required(),
+  password: string().min(8).matches(/(?=.*[0-9])/, 'Password must contain at least one number')
+})
 
 export default function Users() {
 
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    instance('users?_limit=9')
-      .then(res => setUsers(res.data))
-
-    return () => {
-      console.log('the end');
+  const handleSubmit = (values, { resetForm }) => {
+    const user = {
+      id: nanoid(6),
+      ...values
     }
-  }, [])
 
-  const deleteUser = (id) => {
-    setUsers(prevUsers => {
-      return prevUsers.filter(user => user.id !== id)
-    })
+    setUsers([...users, user])
+
+    resetForm();
   }
-
-  const [clicked, setClicked] = useState(null);
-
-
-  const handleClick = (id) => {
-    if (clicked === id) {
-      setClicked(null);
-    } else {
-      setClicked(id);
-    }
-  }
-
-
 
   return (
     <div className="Users">
-      <h1>User List</h1>
-      {
-        users.map(user => {
-          return (
-            <div className={`Users__card ${user.id === clicked ? 'active' : ''}`}
-              key={user.id}
-              onClick={() => handleClick(user.id)}>
-              <h3>{user.name}</h3>
-              <p>{user.email}</p>
-              <button
-                disabled={user.id === clicked ? false : true}
-                className={`${user.id !== clicked ? 'disabled' : 'active'}`}
-                onClick={() => deleteUser(user.id)}>
-                Delete User
-              </button>
-            </div>
-          )
-        })
-      }
+      <h1>Log In</h1>
+      <Formik
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+        initialValues={initialValues}
+      >
+        <Form className="Users__formik">
+          <Field type="text" name="username" placeholder="Your Username" required />
+          <ErrorMessage name="username" component="p" className='errorMessage' />
+          <Field type="password" name="password" placeholder="Your Password" required />
+          <ErrorMessage name="password" component="p" className='errorMessage' />
+          <a href="#">Forgot Your Password?</a>
+          <div className="Users__buttons">
+            <Field type="submit" name="submit" value="Sign In" />
+            <button>Sign Up</button>
+          </div>
+        </Form>
+      </Formik>
+
+      <div className="Users__box">
+        <h1>User List</h1>
+        {
+          users.map(user => {
+            return (<ul key={user.id} className="Users__list" >
+              <li><span>Username: </span> {user.username}</li>
+              <li><span>Password: </span> {user.password}</li>
+            </ul>)
+          })
+        }
+      </div>
+
     </div>
   )
 
