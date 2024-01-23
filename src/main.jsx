24a26bf3649efececb9ed9187ce5770df5f14890
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-01-23 22:11:50 +0400

    json-server

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './components/ModalEx/App'
+import App from './components/json-server/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
